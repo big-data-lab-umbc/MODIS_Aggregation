a1aec49ddeef39262f9e2b839050a3514d9e4ca3
Jianwu Wang <jianwu@umbc.edu>
2019-09-16 11:59:38 -0400

    add more comments.

--- benchmarking/xarray-dask/monthly-aggregation-file-level-dask.py
+++ benchmarking/xarray-dask/monthly-aggregation-file-level-dask.py
@@ -31,35 +31,48 @@ M06_files = sorted(glob.glob(M06_dir + "MYD06_L2.A2008*"))
 
 t0 = time.time()
 
-#return two 2D(180*360) array, one for cloud pixel count and the other for total pixel count.
-def aggregateOneFileData(x,y):
+
+def aggregateOneFileData(M06_file, M03_file):
+	"""Aggregate one file from MYD06_L2 and its corresponding file from MYD03. Read 'Cloud_Mask_1km' variable from the MYD06_L2 file, read 'Latitude' and 'Longitude' variables from the MYD03 file. Group Cloud_Mask_1km values based on their (lat, lon) grid.
+	Args:
+		M06_file (string): File path for M06_file.
+		M03_file (string): File path for corresponding M03_file.
+		
+	Returns:
+		(cloud_pix, total_pix) (tuple): cloud_pix is an 2D(180*360) numpy array for cloud pixel count of each grid, total_pix is an 2D(180*360) numpy array for total pixel count of each grid.
+	"""
     
     total_pix = np.zeros((180, 360))
     cloud_pix = np.zeros((180, 360))
-    
-    d06 = xr.open_dataset(x, drop_variables="Scan Type")['Cloud_Mask_1km'][:,:,0].values
+    #read 'Cloud_Mask_1km' variable from the MYD06_L2 file, whose shape is (2030, 1354)
+    d06 = xr.open_dataset(M06_file, drop_variables="Scan Type")['Cloud_Mask_1km'][:,:,0].values
+    # sampling data with 1/3 ratio (pick 1st, 4th, 7th, ...) in both latitude and longitude direction. d06CM's shape is (677, 452)
     d06CM = d06[::3,::3]
     ds06_decoded = (np.array(d06CM, dtype = "byte") & 0b00000110) >> 1
-    d03_lat = xr.open_dataset(y,drop_variables=var_list)['Latitude'][:,:].values
-    d03_lon = xr.open_dataset(y,drop_variables=var_list)['Longitude'][:,:].values
+    # shape of d03_lat and d03_lon: (2030, 1354)
+    d03_lat = xr.open_dataset(M03_file,drop_variables=var_list)['Latitude'][:,:].values
+    d03_lon = xr.open_dataset(M03_file,drop_variables=var_list)['Longitude'][:,:].values
     
+    # sampling data with 1/3 ratio, shape of lat and lon: (677, 452), then convert data from 2D to 1D, then add offset to change value range from (-90, 90) to (0, 180) for lat.
     lat = (d03_lat[::3,::3].ravel() + 89.5).astype(int)
     lon = (d03_lon[::3,::3].ravel() + 179.5).astype(int)
     lat = np.where(lat > -1, lat, 0)
     lon = np.where(lon > -1, lon, 0)
-    
+    # increment total_pix by 1 for the grid for each value in (lat, lon).
     for i, j in zip(lat, lon):
             total_pix[i,j] += 1
     
+    # covert ds06_decoded from 2D to 1D, check whether each element is less than or equal to 0, return a tuple whose first element is an 1D arrays of indices of ds06_decoded's elements whose value is less than or equal to 0.  
     index = np.nonzero(ds06_decoded.ravel() <= 0)
-    
+    # get its lat and lon for each cloud pixel.
     cloud_lon = [lon[i] for i in index[0]]
     cloud_lat = [lat[i] for i in index[0]]
-    
+     # increment cloud_pix by 1 for the grid for each value in (cloud_lat, cloud_lon).
     for x, y in zip(cloud_lat, cloud_lon):
         cloud_pix[x,y] += 1  
         
     return cloud_pix, total_pix
+
 
 tt = client.map(aggregateOneFileData, M06_files, M03_files)
 
@@ -68,7 +81,7 @@ cloud_pix_global = np.zeros((180, 360))
 total_pix_global = np.zeros((180, 360))
 #finallist = np.zeros((180, 360))
 
-#add each aggregateOneFileData function call result (namely one day result) to final global 2D result
+#add each aggregateOneFileData function call result (namely one file result) to final global 2D result
 for future, result in as_completed(tt, with_results= True):
     #print(result.shape)
     cloud_pix_global+=result[0]
