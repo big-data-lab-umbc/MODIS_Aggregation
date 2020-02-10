a74875200957b2e77dba93c17369e9311d107bee
Jianwu Wang <wangjianwu@gmail.com>
2020-02-10 14:59:14 -0500

    change worker start conf to be 16 parallel processes and 20 gb memory per process

--- benchmarking/dask-environment-setup/start_worker.sh
+++ benchmarking/dask-environment-setup/start_worker.sh
@@ -5,6 +5,7 @@ ps aux | grep -ie dask-worker | awk '{print $2}' | xargs kill -9
 echo "start new worker"
 date
 hostname
+#start 16 parallel processes and 20 GB per process
 /umbc/xfs1/cybertrn/common/Softwares/anaconda3/bin/dask-worker $1:8786 --nprocs 16 --nthreads 1 --memory-limit 20.00GB --death-timeout 60
 date
 hostname
