b7dce920ed805b9c0118af253b99698f1bcd5a21
Jianyu Zheng <jzheng3@umbc.edu>
2020-02-13 13:50:07 -0500

    MPI Version Update to be the same with Spark

--- benchmarking/MPI/monthly-aggregation-file-level-mpi.py
+++ benchmarking/MPI/monthly-aggregation-file-level-mpi.py
@@ -21,14 +21,6 @@ from mpi4py import MPI
 from netCDF4 import Dataset
 import matplotlib.pyplot as plt
 
-def read_filelist(loc_dir,prefix,yr,day,fileformat):
-	# Read the filelist in the specific directory
-	str = os.popen("ls "+ loc_dir + prefix + yr + day + "*."+fileformat).read()
-	fname = np.array(str.split("\n"))
-	fname = np.delete(fname,len(fname)-1)
-
-	return fname
-
 def read_MODIS(M06_files,M03_files,verbose=False): # READ THE HDF FILE
 	
 	# Read the cloud mask from MYD06_L2 product')
@@ -59,7 +51,7 @@ def read_MODIS(M06_files,M03_files,verbose=False): # READ THE HDF FILE
 
 	return lat,lon,CM
 
-def run_modis_aggre(M06_files,M03_files,NTA_lats,NTA_lons,grid_lon,gap_x,gap_y,fileloop):
+def run_modis_aggre(M06_files,M03_files,fileloop):
 	# This function is the data aggregation loops by number of files
 	fileloop = np.array(fileloop)
 	for j in fileloop:
@@ -116,23 +108,8 @@ if __name__ =='__main__':
 	M03_files = sorted(glob.glob(M03_dir + "MYD03.A2008*"))
 	
 	#-------------STEP 2: Set up spactial and temporal resolution----------
-	NTA_lats = [-90,90]   #[  0,40] #[-90,90]   #[-30,30]    
-	NTA_lons = [-180,180] #[-40,60] #[-180,180] #[-60,60]  
-	
-	gap_x, gap_y = 1,1 #0.625,0.5
-
-	if ((NTA_lons[-1]-NTA_lons[0])%gap_x != 0) | ((NTA_lats[-1]-NTA_lats[0])%gap_y != 0): 
-		print("Grid size should be dividable by the dimension of the selected region.")
-		print("If you choose the region of latitude  from -40 to 40, then you gird size (gap_y) should be dividable by 80.")
-		print("If you choose the region of longitude from  20 to 35, then you gird size (gap_x) should be dividable by 55.")
-		print("Please try again!")
-		sys.exit()
-
-	grid_lon=np.int((NTA_lons[-1]-NTA_lons[0])/gap_x)
-	grid_lat=np.int((NTA_lats[-1]-NTA_lats[0])/gap_y)
-
-	total_pix = np.zeros((grid_lat, grid_lon))
-	cloud_pix = np.zeros((grid_lat, grid_lon))
+	total_pix = np.zeros((180, 360))
+	cloud_pix = np.zeros((180, 360))
 
 	# Initiate MPI 
 	comm = MPI.COMM_WORLD
